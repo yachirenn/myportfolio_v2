af350f15a28720c32c1ead9360c9fe832d2dcc43
yachirenn <rendysulistyawan11@gmail.com>
2026-05-30 20:30:30 +0700

    create and adding for clickspark effect

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,23 +1,26 @@
-import { Routes, Route } from 'react-router-dom'
 import Header from './components/partials/header'
 import Hero from './pages/Hero'
-// import Projects from './pages/Projects'
+import About from './pages/About'
+import ClickSpark from './components/CustomCursor'
 
 function App() {
-  // const location = useLocation()
-  // const isHome = location.pathname === '/'
-
   return (
-    <>
+    <ClickSpark
+      sparkColor='#2C1810'
+      sparkSize={8}
+      sparkRadius={20}
+      sparkCount={6}
+      duration={450}
+      easing='ease-out'
+      extraScale={1.2}
+    >
       <Header />
-      
+
       <main className="w-full h-auto">
-        <Routes>
-          <Route path="/" element={<Hero />} />
-          {/* <Route path="/projects" element={<Projects />} /> */}
-        </Routes>
+        <Hero />
+        <About />
       </main>
-    </>
+    </ClickSpark>
   )
 }
 
