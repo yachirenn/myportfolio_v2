56585d6b0251a96a759ff6624e3cbd1c878bd3f8
yachirenn <rendysulistyawan11@gmail.com>
2026-05-30 20:29:08 +0700

    add plugin for lanyard component

---
+++ frontend/src/vite-env.d.ts
@@ -0,0 +1,18 @@
+/// <reference types="vite/client" />
+
+declare module '*.glb';
+declare module '*.png';
+
+declare module 'meshline' {
+  export const MeshLineGeometry: any;
+  export const MeshLineMaterial: any;
+}
+
+declare global {
+  namespace JSX {
+    interface IntrinsicElements {
+      meshLineGeometry: any;
+      meshLineMaterial: any;
+    }
+  }
+}
